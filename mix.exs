0f451c7e8b9fd59d001153b20dbc66f55412f988
Dave Keen <dev@ruffness.com>
2021-08-07 12:43:30 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScopedPolicy.MixProject do
   def project do
     [
       app: :scoped_policy,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
